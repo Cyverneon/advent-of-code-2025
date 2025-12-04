594ac92967e7f412e2e622c1c6bf4b27531e4ade
Cyvern <cyverneon@gmail.com>
2025-12-04 22:31:19 +0000

    Complete day 1 part 2

--- 01.cpp
+++ 01.cpp
@@ -11,15 +11,11 @@ public:
     void calculatePassword(std::string input_filename)
     {
         std::vector<int> rotations = getRotationsFromInput(input_filename);
-        std::cout << makeRotations(rotations);
+        std::cout << "part one: " << countZeros(rotations, false) << std::endl;
+        std::cout << "part two: " << countZeros(rotations, true) << std::endl;
     }
 
 private:
-    int mod(int k, int n)
-    {
-        return ((k%n)+n)%n;
-    }
-
     std::vector<int> getRotationsFromInput(std::string filename)
     {
         FileReader file(filename);
@@ -29,34 +25,39 @@ private:
         for (int i = 0; i < input_lines.size(); i++)
         {
             if (input_lines[i].substr(0, 1) == "R")
-            {
                 rotations.push_back(stoi(input_lines[i].substr(1)));
-            }
             else
-            {
                 rotations.push_back(-stoi(input_lines[i].substr(1)));
-            }
         }
 
         return rotations;
     }
 
-    int makeRotations(std::vector<int> rotations)
+    int countZeros(std::vector<int> rotations, bool includeOverlaps)
     {
         int dial_pos = 50;
         int zero_counter = 0;
         for (int i = 0; i < rotations.size(); i++)
         {
-            //c++'s modulus operator is really a remainder operator and cannot cap negative values to within the desired range
-            // so left rotations which might produce negative results are converted to right rotations 
+            int val;
             if (rotations[i] < 0)
-                dial_pos += (100+rotations[i]);
+            {
+                if (dial_pos == 0)
+                    dial_pos = 100;
+                val = (100-dial_pos) - rotations[i];
+                dial_pos = 100 - (val % 100);
+                if (dial_pos == 100)
+                    dial_pos = 0;
+            }
             else
-                dial_pos += rotations[i];
-
-            dial_pos %= 100;
-            if (dial_pos == 0)
-                zero_counter++;
+            {
+                val = dial_pos + rotations[i];
+                dial_pos = val % 100;
+            }
+            if (includeOverlaps)
+                zero_counter += val / 100;
+            else
+                zero_counter += (dial_pos == 0);
         }
         return zero_counter;
     }
@@ -66,6 +67,5 @@ int main()
 {
     Day01 day01;
     day01.calculatePassword("01_input.txt");
-
     return 0;
 }

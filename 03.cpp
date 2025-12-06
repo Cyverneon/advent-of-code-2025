625f982181cd3f8c96efec49fc3aa71694ad8ef1
Cyvern <cyverneon@gmail.com>
2025-12-06 01:37:16 +0000

    complete day 3 part 2

--- 03.cpp
+++ 03.cpp
@@ -8,24 +8,27 @@ public:
     Day03() {}
     ~Day03() {}
 
-    int calculateMaxOutputVoltage(std::string input_filename, int batteries)
+    long long calculateMaxOutputVoltage(std::string input_filename, int batteries)
     {
         FileReader file(input_filename);
         std::vector<std::string> battery_banks = file.getLines();
-        int total = 0;
+        long long total = 0;
         for (int i = 0; i < battery_banks.size(); i++)
         {
-            int highest_pos = getPosOfHighestDigit(battery_banks[i].substr(0, battery_banks[i].size()-1));
-            std::string joltage = battery_banks[i].substr(highest_pos, 1);
-            int next_highest_pos = getPosOfHighestDigit(battery_banks[i].substr(highest_pos+1));
-            joltage.append(battery_banks[i].substr(highest_pos + next_highest_pos + 1, 1));
-            total += stoi(joltage);
+            int highest_pos = 0;
+            std::string joltage = "";
+            for (int j = 0; j < batteries; j++)
+            {
+                highest_pos = highest_pos + getPosOfHighestDigit(battery_banks[i].substr(highest_pos, battery_banks[i].size() - batteries - highest_pos + j + 1));
+                joltage.append(battery_banks[i].substr(highest_pos, 1));
+                highest_pos++;
+            }
+            total += stoll(joltage);
         }
         return total;
     }
 
 private:
-
     int getPosOfHighestDigit(std::string str)
     {
         int highest_pos = 0;
@@ -45,7 +48,7 @@ private:
 int main()
 {
     Day03 day03;
-    std::cout << day03.calculateMaxOutputVoltage("03_input.txt");
-
+    std::cout << "part 1: " << day03.calculateMaxOutputVoltage("03_input.txt", 2) << std::endl;
+    std::cout << "part 2: " << day03.calculateMaxOutputVoltage("03_input.txt", 12) << std::endl;
     return 0;
 }
